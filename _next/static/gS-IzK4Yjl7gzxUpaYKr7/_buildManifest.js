658f17584415100dcb169b420826cf5ba3d012d3
shizoe <mabombebeta@gmail.com>
2021-10-12 13:10:46 +0200

    Updates

--- _next/static/gS-IzK4Yjl7gzxUpaYKr7/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/159-3cdb7815374fd0402bed.js","static/chunks/pages/index-e545f0fbdbde3441f29c.js"],"/_error":["static/chunks/pages/_error-9faf4177fb4e528b4124.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
